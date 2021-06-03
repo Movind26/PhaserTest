b018e9ddd164b1c302ec3603c88ab010a4b77bf6
Movind26 <movind26@protonmail.ch>
2021-06-03 11:20:25 +0300

    Added and optimized the code

---
+++ src/config.js
@@ -0,0 +1,11 @@
+import Phaser from 'phaser'
+import GameScene from './GameScene'
+
+const config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    scene: GameScene
+}
+export { config }
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { Game } from 'phaser';
+
+import { config } from './config'
+
+var game = new Game(config);
